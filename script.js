2e6aab1e34ed35f41151c0ea6546c4e2a8277b64
wabemkt-coder <wabemkt@gmail.com>
2026-01-16 11:38:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Liquid Glass starter loaded")
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 400) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
